d40bc74e7c894f8a978cbf32b20d00574b1b7d46
DavidGau <gaulindavidweb@gmail.com>
2018-04-14 11:27:01 -0400

    Deleted connectionBdd

--- php/connectionDb.php
+++ php/connectionDb.php
@@ -1,7 +1,7 @@
 <?php
 	try{
-		$db = new PDO("mysql:host=localhost;dbname=hackDon","root","root",array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-	}catch(PDOexception $e){
+		$db = new PDO("mysql:host=localhost;dbname=hackdon","root","",array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	}catch(PDOException $e){
 		echo "Can't reach db";
 		
 	}
