d865bf6f0470e0dd25c15ef61c0ce58601bd04c0
DavidGau <gaulindavidweb@gmail.com>
2018-04-14 10:50:44 -0400

    Added signUp for org

--- php/connectionDb.php
+++ php/connectionDb.php
@@ -2,7 +2,7 @@
 	try{
 		$db = new PDO("mysql:host=localhost;dbname=hackDon","root","root",array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 	}catch(PDOexception $e){
-		throw new PDOException("Can't reach de Db".$e -> getMessage());
+		echo "Can't reach db";
 		
 	}
 ?>
