cbc8d31b73fa58a44350e2db010c5e3752e2a82c
Andlat <Andlat@users.noreply.github.com>
2018-04-14 11:41:47 -0400

    fonction en wrapper

--- php/connectionDb.php
+++ php/connectionDb.php
@@ -1,8 +1,11 @@
 <?php
-	try{
-		$db = new PDO("mysql:host=localhost;dbname=hackDon","root","root",array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-	}catch(PDOexception $e){
-		echo "Can't reach db";
-		
+	function connectDB(){
+		try{
+			$db = new PDO("mysql:host=localhost;dbname=hackDon","root","root",array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		}catch(PDOexception $e){
+			die("Can't reach db");
+		}
+
+		return $db;
 	}
 ?>
