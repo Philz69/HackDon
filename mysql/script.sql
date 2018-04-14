dffd5257710c2876332a85a6cfecf5c1181870f8
Andlat <Andlat@users.noreply.github.com>
2018-04-14 19:46:53 -0400

    notifications de base
    Notifications sur le site web. N'affiche pas celles de tes anciennes visites. Manque les notifications push pour chrome et firefox

--- mysql/script.sql
+++ mysql/script.sql
@@ -10,7 +10,7 @@ CREATE TABLE accountOrganisation(
 	name VARCHAR(255) NOT NULL,
 	email VARCHAR(255) NOT NULL,
 	passwordHash VARCHAR(255) NOT NULL,
-	description BLOB NOT NULL,
+	description TEXT NOT NULL,
 	bannerImg BLOB DEFAULT NULL,
 	code VARCHAR(255) NOT NULL,
 	datetime DATETIME DEFAULT NOW() NOT NULL,
@@ -31,7 +31,7 @@ CREATE TABLE project(
 	ID INT AUTO_INCREMENT NOT NULL,
 	organisationID INT NOT NULL,
 	name VARCHAR(255) NOT NULL,
-	description BLOB NOT NULL,
+	description TEXT NOT NULL,
 	objective VARCHAR(255) NOT NULL,
 	amountWanted INT NOT NULL,
 	amountCollected INT DEFAULT 0 NOT NULL,
@@ -45,7 +45,7 @@ CREATE TABLE donations(
 	ID INT AUTO_INCREMENT NOT NULL,
 	accountID INT NOT NULL,
 	projectID INT NOT NULL,
-	amountID INT DEFAULT 0 NOT NULL,
+	amount INT DEFAULT 0 NOT NULL,
 	datetime DATETIME DEFAULT NOW() NOT NULL,
 	PRIMARY KEY(ID),
 	CONSTRAINT FOREIGN KEY(accountID) REFERENCES accountUser(ID) ON DELETE CASCADE,
@@ -57,15 +57,25 @@ CREATE TABLE notifications(
 	accountID INT NOT NULL,
 	projectID INT NOT NULL,
 	content VARCHAR(255) NOT NULL,
-	isSeen BOOLEAN DEFAULT FALSE NOT NULL,
+	isFetched BOOLEAN DEFAULT FALSE NOT NULL,
 	datetime DATETIME DEFAULT NOW() NOT NULL,
 	PRIMARY KEY(ID),
 	CONSTRAINT FOREIGN KEY(accountID) REFERENCES accountUser(ID) ON DELETE CASCADE,
 	CONSTRAINT FOREIGN KEY(projectID) REFERENCES project(ID) ON DELETE CASCADE
 )Engine=InnoDB;
 
+CREATE TABLE publications(
+	ID INT AUTO_INCREMENT NOT NULL,
+	orgID NOT NULL,
+	projectID INT NOT NULL,
+	message TEXT NOT NULL,
+	datetime DATETIME DEFAULT NOW() NOT NULL
+)Engine=InnoDB;
+
 
 /*** RECORDS FOR TESTING ***/
 INSERT INTO accountUser(name, email, passwordHash) VALUES("bob", "bob@bob.com", "");
 INSERT INTO accountOrganisation(name, email, passwordHash, description, code) VALUES("CHUS", "fondation@chus.org", "", "Fondation du CHUS", "qwerty");
-INSERT INTO project(organisationID, name, description, amountWanted) VALUES(1, "Cancer", "Rammasser de l'argent pour aider le cancer.", 3000000);
+INSERT INTO project(organisationID, name, description, objective, amountWanted) VALUES(1, "Cancer", "Rammasser de l'argent pour aider le cancer.", "TUER le cancer", 3000000);
+INSERT INTO notifications(accountID, projectID, content) VALUES(1, 1, "Your donations of 1$ saved 32 people!");
+INSERT INTO notifications(accountID, projectID, content) VALUES(1, 1, "Please donate more!!!!!");
--- php/sendNotifications.php
+++ php/sendNotifications.php
@@ -2,15 +2,17 @@
 	require_once("connectionDb.php");
 
 	function sendNotification($accountID, $projectID, $content){
-		$db = connectDB();
-		$query = $db->prepare("INSERT INTO notifications(accountID, projectID, content) VALUES(:accountID, :projectID, :notification);");
-		$query->bindParam(":accountID", $accountID);
-		$query->bindParam(":projectID", $projectID);
-		$query->bindParam(":notification", $content);
+		if($accountID!=null && $projectID!=null && $content!=null){
+				$db = connectDB();
+				$query = $db->prepare("INSERT INTO notifications(accountID, projectID, content) VALUES(:accountID, :projectID, :notification);");
+				$query->bindParam(":accountID", $accountID);
+				$query->bindParam(":projectID", $projectID);
+				$query->bindParam(":notification", $content);
 
-		$query->execute();
+				$query->execute();
 
-		$db = null;
+				$db = null;
+			}
 	}
 
 	//Ajouter la notification
