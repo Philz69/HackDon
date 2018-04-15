8fff11b0a0b8c7d3c57c902815bd8973c37c1abe
Philipe Goulet <phil.gou69@hotmail.com>
2018-04-15 00:03:31 -0400

    Finished login buttons

--- js/popuphandler.js
+++ js/popuphandler.js
@@ -1,18 +1,57 @@
 $( document ).ready(function() {
-    $('#connectUser').hide();
-    $('#connectOrg').hide();
+    $('#loginUsr').hide();
+    $('#loginOrg').hide();
     $('#signupOrg').hide();
     $('#signupbtn').hide();
     $('#usrswitchbtn').hide();
     
+    $('#loginbtn').click(function() {
+            $('#loginbtn').hide();
+            $('#signupbtn').show();
+        if($('#signupOrg').css('display') == 'none') {
+            alert('hide usr signup');
+            $('#signupUsr').hide();
+            $('#loginUsr').show();
+        }
+        else {
+           $('#signupOrg').hide();
+           $('#loginOrg').show();
+        }
+    });
+    $('#signupbtn').click(function() {
+            $('#signupbtn').hide();
+            $('#loginbtn').show();
+        if($('#loginOrg').css('display') == 'none') {
+            $('#loginUsr').hide();
+            $('#signupUsr').show();
+        }
+        else {
+           $('#signupOrg').show();
+           $('#loginOrg').hide();
+        }
+    });
+    $('#usrswitchbtn').click(function() {
+            $('#usrswitchbtn').hide();
+            $('#orgswitchbtn').show();
+        if($('#loginOrg').css('display') == 'none') {
+            $('#signupOrg').hide();
+            $('#signupUsr').show();
+        }
+        else {
+           $('#loginOrg').hide();
+           $('#loginUsr').show();
+        }
+    });
     $('#orgswitchbtn').click(function() {
-        if($('#connectUser').css('display') == 'none') {
-            $('#signupUser').hide();
+            $('#orgswitchbtn').hide();
+            $('#usrswitchbtn').show();
+        if($('#loginUsr').css('display') == 'none') {
+            $('#signupUsr').hide();
             $('#signupOrg').show();
         }
         else {
-           $('#connectUser').hide();
-           $('#connectOrg').show();
+           $('#loginUsr').hide();
+           $('#loginOrg').show();
         }
     });
 
