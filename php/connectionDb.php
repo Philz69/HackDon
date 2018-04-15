b087fc55cdf0ba502758db4b790b5c75306fb747
DavidGau <gaulindavidweb@gmail.com>
2018-04-14 22:06:05 -0400

    Debug du connexion

--- php/connectionDb.php
+++ php/connectionDb.php
@@ -2,7 +2,7 @@
 	function connectDB(){
 		try{
 
-			$db = new PDO("mysql:host=localhost;dbname=hackDon","root","root",array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+			$db = new PDO("mysql:host=localhost;dbname=hackDon","root","rroot",array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
 		}catch(PDOexception $e){
 			die("Can't reach db");
